55107a1f69842155ed5706cc83fa228ad038df4b
Bas Outlawz <bas@outlawz.nl>
2024-07-02 21:05:34 +0200

    fix: small footer fixes
    changed the footer links for services, made the ul style only apply to main div and unhidden the website footer column on laptop or larger, this must be a design flaw

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -4,16 +4,17 @@
         <div class="grid grid-cols-1 gap-12 pt-[35px] uppercase lg:grid-cols-4">
             @if ($logo = get_field('logo', 'option'))
                 <a class="mx-auto block w-full md:mx-0" href="{{ home_url('/') }} order-1">
-                    <img class="h-full max-h-[92px] w-full max-w-[312px] object-contain" src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}">
+                    <img class="h-full max-h-[92px] w-full max-w-[312px] object-contain" src="{{ $logo['url'] }}"
+                        alt="{{ $logo['alt'] }}">
                 </a>
                 @if (has_nav_menu('header_navigation'))
-                    <nav class="order-2 hidden space-y-[6px] lg:block" aria-label="footer">
+                    <nav class="order-2 space-y-[6px]" aria-label="footer">
                         <p class="font-lato text-footer-title text-gold-2 uppercase">Website</p>
                         {!! wp_nav_menu([
                             'theme_location' => 'header_navigation',
                             'menu_class' => 'list-none font-lato text-white text-button space-y-[6px]',
                             'link_class' => 'text-[#030712]',
-                            'echo' => false
+                            'echo' => false,
                         ]) !!}
                     </nav>
                 @endif
@@ -23,7 +24,8 @@
                         <ul class="list-none space-y-[6px]">
                             @foreach ($services as $service)
                                 <li>
-                                    <a class="font-lato text-button text-white" href="{{ get_the_permalink($service) }}">{{ $service->post_title }}</a>
+                                    <a class="font-lato text-button text-white"
+                                        href="/diensten#{{ $service->post_name }}">{{ $service->post_title }}</a>
                                 </li>
                             @endforeach
                         </ul>
@@ -45,12 +47,14 @@
                             @endif
                             @if ($contact_arr['email'])
                                 <li>
-                                    <a class="font-lato text-button text-white" href="mailto:{{ $contact_arr['email'] }}">{{ $contact_arr['email'] }}</a>
+                                    <a class="font-lato text-button text-white"
+                                        href="mailto:{{ $contact_arr['email'] }}">{{ $contact_arr['email'] }}</a>
                                 </li>
                             @endif
                             @if ($contact_arr['phone'])
                                 <li>
-                                    <a class="font-lato text-button text-white" href="tel:{{ $contact_arr['phone']['url'] }}">{{ $contact_arr['phone']['title'] }}</a>
+                                    <a class="font-lato text-button text-white"
+                                        href="tel:{{ $contact_arr['phone']['url'] }}">{{ $contact_arr['phone']['title'] }}</a>
                                 </li>
                             @endif
                         </ul>
