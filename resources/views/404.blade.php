b7c66d02291d74174b8ddaef7daf248f79769f70
Bas Outlawz <bas@outlawz.nl>
2024-07-03 22:36:31 +0200

    feat: made 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.page-header')
+    <section class="-mb-10 h-screen w-full place-content-center bg-cover bg-bottom"
+        style="background-image: url(@asset('images/login-background.png'))">
+        <x-container>
 
-    @if (!have_posts())
-        <x-alert type="warning">
-            {!! __('Sorry, but the page you are trying to view does not exist.', 'radicle') !!}
-        </x-alert>
+            <h1 class="text-title-large text-gold md:text-[100px] font-bold">{{ __('404', 'outlawz') }}</h1>
+            <span class="text-title-small md:mt-10 mb-4 block text-white">{{ __('Oeps! Deze pagina bestaat niet', 'outlawz') }}</span>
+            <x-button href="{{ home_url() }}">{{ __('Terug naar de homepage', 'outlawz') }}</x-button>
+        </x-container>
 
-        {!! get_search_form(false) !!}
-    @endif
+    </section>
 @endsection
