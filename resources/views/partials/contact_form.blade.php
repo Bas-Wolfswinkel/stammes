b294ea56e729cf649da826986f3e3fc5bd842806
Bas Outlawz <bas@outlawz.nl>
2024-07-21 16:41:51 +0200

    feat: added google maps

--- resources/views/partials/contact_form.blade.php
+++ resources/views/partials/contact_form.blade.php
@@ -8,7 +8,7 @@
             'address' => get_field('address', 'option') ?? null,
             'email' => get_field('email', 'option') ?? null,
             'phone' => get_field('phone', 'option') ?? null,
-            'postal_city' => get_field('postal_city', 'option') ?? null
+            'postal_city' => get_field('postal_city', 'option') ?? null,
         ];
     }
     $right = get_sub_field('contact_form_right');
@@ -24,7 +24,8 @@
                     <div class="@if ($color) mb-[50px] @endif relative">
                         <img class="max-h-[808px]" src="{{ $image['url'] }}" alt="{{ $image['alt'] }}">
                         @if ($color)
-                            <div class="pointer-events-none absolute bottom-0 left-0 -z-10 h-[258px] max-h-full w-[461px] max-w-full -translate-x-[50px] translate-y-[50px]" style="background-color: {{ $color }}">
+                            <div class="pointer-events-none absolute bottom-0 left-0 -z-10 h-[258px] max-h-full w-[461px] max-w-full -translate-x-[50px] translate-y-[50px]"
+                                style="background-color: {{ $color }}">
                             </div>
                         @endif
                     </div>
@@ -73,7 +74,8 @@
                 <div class="@if ($color) mb-[35px] @endif relative mt-[51px] w-full lg:hidden">
                     <img class="ml-auto max-w-[90%]" src="{{ $image['url'] }}" alt="{{ $image['alt'] }}">
                     @if ($color)
-                        <div class="absolute bottom-0 left-0 -z-10 h-[168px] w-[80%] translate-y-[35px]" style="background-color: {{ $color }}">
+                        <div class="absolute bottom-0 left-0 -z-10 h-[168px] w-[80%] translate-y-[35px]"
+                            style="background-color: {{ $color }}">
                         </div>
                     @endif
                 </div>
@@ -84,3 +86,8 @@
         </div>
     </div>
 </x-container>
+
+<iframe class="-mb-20 mt-10"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2418.8187260174363!2d4.794449576316321!3d52.68131117210378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf56b0af088505%3A0xe4c4e27be2ee5e0a!2sSchoenmakersloot%206%2C%201721%20HR%20Broek%20op%20Langedijk!5e0!3m2!1snl!2snl!4v1721572687220!5m2!1snl!2snl"
+    style="border:0;" width="100%" height="450" allowfullscreen="" loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
